95902c140e8dfca2a42fb5f17bcc9bd333a60624
baezitr <baezitr@gmail.com>
2025-08-30 20:08:20 -0300

    Arquivos base

---
+++ Data.java
@@ -0,0 +1,97 @@
+public class Data
+{
+    private byte  dia, mes;
+    private short ano;
+
+    public static boolean isBissexto (short ano)
+    {
+        // Calendario Juliano
+        if (ano<1582)
+            if (ano%4==0)
+                return true;
+            else
+                return false;
+
+        // Calendario Gregoriano
+        if (ano%400==0) return true;
+        if (ano%  4==0 && ano%100!=0) return true;
+        return false;
+    }
+
+    public static boolean isValida (byte dia, byte mes, short ano)
+    {
+        if (ano<-45) return false; // antes do Calendario Juliano
+        if (ano== 0) return false; // nao existiu ano 0; do ano 1ac foi direto para o ano 1dc
+        if (ano==1582 && mes==10 && dia>=5 && dia<=14) return false; // dias cortados dos calendario pelo Papa Gregorio
+
+        if (dia<1 || dia>31 || mes<1 || mes>12) return false;
+
+        if (dia>30 && (mes==4 || mes==6 || mes==9 || mes==11)) return false;
+        if (dia>29 && mes==2) return false;
+        if (dia>28 && mes==2 && !Data.isBissexto(ano)) return false;
+
+        return true;
+    }
+
+    public /*void*/ Data (byte dia, byte mes, short ano) throws Exception
+    {
+        if (!Data.isValida(dia,mes,ano))
+            throw new Exception ("Data invalida");
+
+        this.dia=dia;
+        this.mes=mes;
+        this.ano=ano;
+    }
+
+    public void setDia (byte dia) throws Exception
+    {
+        if (!Data.isValida(dia,this.mes,this.ano))
+            throw new Exception ("Dia invalido");
+
+        this.dia=dia;
+    }
+
+    public byte getDia ()
+    {
+        return this.dia;
+    }
+    
+    public void setMes (byte mes) throws Exception
+    {
+        if (!Data.isValida(this.dia,mes,this.ano))
+            throw new Exception ("Mes invalido");
+
+        this.mes=mes;
+    }
+
+    public byte getMes ()
+    {
+        return this.mes;
+    }
+    
+    public void setAno (short ano) throws Exception
+    {
+        if (!Data.isValida(this.dia,this.mes,ano))
+            throw new Exception ("Ano invalido");
+
+        this.ano=ano;
+    }
+
+    public short getAno ()
+    {
+        return this.ano;
+    }
+    
+    public void avanceUmDia () // altera o this
+    {
+        // faça       
+    }
+
+    public Data getDiaSeguinte () // não altera o this
+    {
+        // faça
+    }
+
+}
+
+// faça uma classe Main para testar
---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        try
+        {
+            Data d1 = new Data ((byte)12,(byte)8,(short)2025);
+            System.out.println (d1.getDia()+"/"+d1.getMes()+"/"+d1.getAno()); // 12/8/2025
+            
+            d1.avanceUmDia();
+            System.out.println (d1.getDia()+"/"+d1.getMes()+"/"+d1.getAno()); // 13/8/2025
+
+            Data d2=d1.getDiaSeguinte();
+            System.out.println (d2.getDia()+"/"+d2.getMes()+"/"+d2.getAno()); // 14/8/2025
+            System.out.println (d1.getDia()+"/"+d1.getMes()+"/"+d1.getAno()); // 13/8/2025
+        }
+        catch (Exception erro)
+        {
+            System.err.println (erro.getMessage());
+        }
+    }
+}
